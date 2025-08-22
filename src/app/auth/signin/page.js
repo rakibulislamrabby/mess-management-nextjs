9f57c83471bfb1f0854f54a0104799fc8f10713a
Md Rakibul Islam <rakibulislamrabby64@gmail.com>
2025-08-22 21:38:30 +0600

    fix bug

--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -84,7 +84,7 @@ export default function SignIn() {
             </Button>
           </form>
           <div className="mt-4 text-center text-sm">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link href="/auth/signup" className="text-primary hover:underline">
               Sign up
             </Link>
